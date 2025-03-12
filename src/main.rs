875251343b49e53e479650dd2bc4151793f8d744
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-03-11 22:17:09 -0300

    updating main.rs doc

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,58 @@
 //! publishable taking on account both version string and checksum.
 //!
 //!
+//! ## Install
+//!
+//! `cargo install cargo-caw-publish`
+//!
+//!
 //! ## Usage
 //!
-//! - if you don't a crate/package name it will be assumed you want to process
-//! the Cargo.toml file at the root of the current folder.
+//! - verify the Cargo.toml file at the root of the current folder.
 //!
 //! ```bash
 //! $ cargo caw-publish
 //! ```
 //!
-//! - if passing crate/package name it will be assumed you want to process
-//! a Cargo.toml file at <package_name>/Cargo.toml
+//! - verify the Cargo.toml file at a path `<package_name>/Cargo.toml`
 //!
 //! ```bash
 //! $ cargo caw-publish <package_name>
 //! ```
 //!
-//! - if you need to pass extra arguments for the "cargo package" phase
+//! - pass extra arguments for the "cargo package" phase
 //!
 //! ```bash
 //! $ cargo caw-publish --package-args="--allow-dirty --keep-going"
 //! ```
 //!
-//! - if you need to pass extra arguments for the "cargo publish" phase
+//! - pass extra arguments for the "cargo publish" phase
 //!
 //! ```bash
 //! $ cargo caw-publish --publish-args="--all-features --keep-going"
+//! ```
+//!
+//! ## Outputs
+//!
+//! Let's say our latest release was the version 1.2.3. So:
+//!
+//! - when you try to re-run publish with the same version we will return
+//! (exit code 0):
+//!
+//! ```
+//! the version <ver_string> is already published at the remote registry. nothing
+//! to do here.
+//! ```
+//!
+//! - when you change some code and forgets to update the version string we will
+//! return (exit code 1):
+//!
+//! ```
+//! the version <ver_string> is already published at the remote registry but your
+//! local .crate checksum differs from the one on the remote:
+//! local : <hash-a>
+//! remote: <hash-b>
+//! hint: maybe you forgot to update the version at <ver_string>!?
 //! ```
 
 use std::{
