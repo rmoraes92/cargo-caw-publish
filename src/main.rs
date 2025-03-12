3174918af86a2d47953bdbcfdec9091eeb22f9dd
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-03-11 22:03:20 -0300

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,53 @@
 //! # cargo-caw-publish (checksum aware wrapper publish)
 //! A thin wrapper around `cargo publish` that verifies if a crate is
 //! publishable taking on account both version string and checksum.
-//! 
-//! 
+//!
+//!
 //! ## Usage
-//! 
+//!
 //! - if you don't a crate/package name it will be assumed you want to process
 //! the Cargo.toml file at the root of the current folder.
-//! 
+//!
 //! ```bash
 //! $ cargo caw-publish
 //! ```
-//! 
+//!
 //! - if passing crate/package name it will be assumed you want to process
 //! a Cargo.toml file at <package_name>/Cargo.toml
-//! 
+//!
 //! ```bash
 //! $ cargo caw-publish <package_name>
 //! ```
-//! 
+//!
 //! - if you need to pass extra arguments for the "cargo package" phase
-//! 
+//!
 //! ```bash
 //! $ cargo caw-publish --package-args="--allow-dirty --keep-going"
 //! ```
-//! 
+//!
 //! - if you need to pass extra arguments for the "cargo publish" phase
-//! 
+//!
 //! ```bash
 //! $ cargo caw-publish --publish-args="--all-features --keep-going"
 //! ```
 
-use std::{path::{Path, PathBuf}, process};
+use std::{
+    path::{Path, PathBuf},
+    process,
+};
 
 use clap::Parser;
 
-use log::{info, debug, error};
+use log::{debug, error, info};
 use serde::Deserialize;
 use sha256::try_digest;
 
-use cargo_caw_publish::{init_logger, CargoWrapper};
-use cargo_caw_publish::CargoToml;
-use cargo_caw_publish::exec_cargo_version;
 use cargo_caw_publish::exec_cargo_package;
 use cargo_caw_publish::exec_cargo_publish;
+use cargo_caw_publish::exec_cargo_version;
 use cargo_caw_publish::get_crate_data as get_cratesio_data;
+use cargo_caw_publish::CargoToml;
+use cargo_caw_publish::{init_logger, CargoWrapper};
 
 #[derive(Debug, Deserialize, Clone)]
 struct CrateVersion {
@@ -61,10 +64,9 @@ fn get_sha256_from_crate_file(crate_file_path: &Path) -> String {
     try_digest(crate_file_path).unwrap()
 }
 
-
 fn main() {
     let cli = match CargoWrapper::parse() {
-      CargoWrapper::CawPublish(c) => c,
+        CargoWrapper::CawPublish(c) => c,
     };
     // let cli = Cli::parse();
 
@@ -82,7 +84,7 @@ fn main() {
         Err(e) => {
             error!("err while loading crate's toml: {}", e);
             process::exit(1);
-        },
+        }
     };
     let crate_ver = cargo_toml.package.version;
     let crate_name = cargo_toml.package.name;
@@ -90,22 +92,25 @@ fn main() {
     // Recalculating Checksum
     // =========================================================================
     match exec_cargo_package(Some(&crate_name), cli.package_args) {
-        Ok(s) => if s.len() > 0 {
-            info!("exec cargo package: {}", s)
-        },
+        Ok(s) => {
+            if s.len() > 0 {
+                info!("exec cargo package: {}", s)
+            }
+        }
         Err(e) => {
             error!("exec cargo package: {}", e);
             process::exit(1);
-        },
+        }
     };
     let crate_file_path = PathBuf::from(format!(
-        "./target/package/{}-{}.crate", crate_name, crate_ver));
+        "./target/package/{}-{}.crate",
+        crate_name, crate_ver
+    ));
     if !crate_file_path.is_file() {
         error!("crate file does not exists: {}", crate_file_path.display());
         process::exit(1);
     }
-    let crate_checksum = get_sha256_from_crate_file(
-        crate_file_path.as_path());
+    let crate_checksum = get_sha256_from_crate_file(crate_file_path.as_path());
 
     // Fetching data from Remote Registry
     // =========================================================================
@@ -114,14 +119,14 @@ fn main() {
             let trimmed = s.trim().to_string();
             debug!("exec cargo version: {}", trimmed);
             trimmed
-        },
+        }
         Err(e) => {
             let fallback_ver = "cargo 1.82.0 (8f40fc59f 2024-08-21)";
             error!("exec cargo version: {}", e);
             error!("exec cargo version: falling back to {}", fallback_ver);
             String::from(fallback_ver)
-        },
-    }; 
+        }
+    };
     let resp = get_cratesio_data(&crate_name, &cargo_ver);
     // TODO make this less nested?
     match resp {
@@ -129,17 +134,19 @@ fn main() {
             if r.status() == 404 {
                 // Crate does NOT have an entry yet at the registry
                 match exec_cargo_publish(Some(&crate_name), cli.publish_args) {
-                    Ok(s) => if s.len() > 0 {
-                        info!("exec cargo publish: {}", s)
-                    },
+                    Ok(s) => {
+                        if s.len() > 0 {
+                            info!("exec cargo publish: {}", s)
+                        }
+                    }
                     Err(e) => {
                         error!("exec cargo publish: {}", e);
                         process::exit(1);
-                    },
+                    }
                 };
-                return
+                return;
             }
-            if r.status() != 200{
+            if r.status() != 200 {
                 // TODO recover HTTP Response body and add to the error message
                 error!("remote registry err: {} - ", r.status());
                 process::exit(1);
@@ -152,7 +159,7 @@ fn main() {
                                 // TODO add a flag to make this scenario return
                                 // 0
                                 info!("the version {} is already published at the remote registry. nothing to do here.", version.num);
-                                return
+                                return;
                             } else {
                                 error!(
                                     "the version {} is already published at \
@@ -172,25 +179,27 @@ fn main() {
                         }
                     }
                     match exec_cargo_publish(Some(&crate_name), cli.publish_args) {
-                        Ok(s) => if s.len() > 0 {
-                            info!("exec cargo publish: {}", s)
-                        },
+                        Ok(s) => {
+                            if s.len() > 0 {
+                                info!("exec cargo publish: {}", s)
+                            }
+                        }
                         Err(e) => {
                             error!("exec cargo publish: {}", e);
                             process::exit(1);
-                        },
+                        }
                     };
-                    return
-                },
+                    return;
+                }
                 Err(e) => {
                     error!("err parsing resp from remote registry: {}", e);
                     process::exit(1);
-                },
+                }
             };
-        },
+        }
         Err(e) => {
             error!("err while attempting to connect to remote registry: {}", e);
             process::exit(1);
-        },
+        }
     };
 }
